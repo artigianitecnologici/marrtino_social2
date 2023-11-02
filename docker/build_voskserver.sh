037160545756277a8517a7cdf5aa21e46921c16b
Fabio Ferrarini <ferrarini09@gmail.com>
2023-11-02 16:27:08 +0100

    vosk-server

--- docker/build_voskserver.sh
+++ docker/build_voskserver.sh
@@ -5,3 +5,4 @@ set -x
 
 docker build --no-cache --build-arg KALDI_MKL=0 --file Dockerfile.kaldi-vosk-server --tag alphacep/kaldi-vosk-server:latest .
 docker build --file Dockerfile.kaldi-it --tag alphacep/kaldi-it:latest .
+docker build -t marrtino:vosk-client -f Dockerfile.vosk-client .

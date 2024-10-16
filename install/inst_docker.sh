0c62eafa8d8931c1f9ca938f7104a830af96becc
Fabio Ferrarini <ferrarini09@gmail.com>
2024-10-16 06:43:12 +0200

    Update Voice

--- install/inst_docker.sh
+++ install/inst_docker.sh
@@ -1,9 +1,5 @@
-echo "https://www.digitalocean.com/community/tutorials/how-to-install-and-use-docker-on-ubuntu-20-04"
-sudo apt install apt-transport-https ca-certificates curl software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
-apt-cache policy docker-ce
-sudo apt install docker-ce
+echo "install DOcker"
+sudo apt install docker-io docker-compose
 sudo systemctl status docker
 
 echo "--------"
